cb43387473d8389ccf54591aa8a006bb6e0d9b74
Melih <melihsahtiyan@gmail.com>
2022-03-23 11:39:00 +0300

    Assignment 4 23.03.2022

---
+++ app/src/main/java/com/syntax/assignment4/MainActivity.java
@@ -0,0 +1,46 @@
+package com.syntax.assignment4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    private TextView dateText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dateText = findViewById(R.id.date_text);
+
+        findViewById(R.id.show_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
+    }
+
+    public void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date = "Your birth date is :\n" + "Day :" + dayOfMonth + "\nMonth :" + month + "\nYear :" + year;
+        dateText.setText(date);
+    }
+}
